40c17b30dcec3669c4e385013df92b1567dd7771
James Jackson <james@pop-os.localdomain>
2023-12-27 12:43:45 -0600

    Added rigidbodies to walls and floor.
    Updated dependencies, because the previous verision of bevy_rapier3d wasn't compatible with bevy v0.12.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 use bevy::prelude::*;
+use bevy_rapier3d::prelude::*;
 use maze::Maze;
 use std::f32::consts::PI;
-
 mod level;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_plugins(level::Level::default())
+        .add_plugins(RapierPhysicsPlugin::<NoUserData>::default())
+        //        .add_plugins(RapierDebugRenderPlugin::default())
         .run();
 }
