5e4378a208119930e0d8b9e9ed37addfbe405e1f
James Jackson <j.e.m.jackson@gmail.com>
2024-05-19 16:35:50 -0500

    Bugs despawn when shot.

--- src/bullet.rs
+++ src/bullet.rs
@@ -1,6 +1,8 @@
 use bevy::prelude::*;
 use bevy_xpbd_3d::prelude::*;
 
+use crate::enemy::Enemy;
+
 const BULLET_SPEED: f32 = 20.0;
 
 #[derive(Component)]
@@ -26,10 +28,17 @@ pub fn spawn_bullet(
         .insert(Sensor);
 }
 
-pub fn hit_bullet(mut commands: Commands, query: Query<(&Bullet, Entity, &CollidingEntities)>) {
+pub fn hit_bullet(
+    mut commands: Commands,
+    query: Query<(&Bullet, Entity, &CollidingEntities)>,
+    enemy_query: Query<(&Enemy, Entity)>,
+) {
     for (_bullet, bullet_entity, colliders) in query.iter() {
         if colliders.len() > 0 {
             commands.entity(bullet_entity).despawn_recursive();
+            for (_, enemy_entity) in enemy_query.iter().filter(|(_, ee)| colliders.contains(ee)) {
+                commands.entity(enemy_entity).despawn_recursive();
+            }
         }
     }
 }
